f17b11a19eee4fddece917d47d4dce72f7ccaccd
SoubarnikaS <soubarnikasankareswaran@gmail.com>
2023-12-12 00:05:39 +0530

    Day3 HomePage

--- Day3/myapp/src/Components/Home.js
+++ Day3/myapp/src/Components/Home.js
@@ -1,30 +1,86 @@
 import wp from '../Assests/Images/wp.jpg'
- 
+
 function Home() {
     return (
         <>
-            <div className = "Phrase">
-                <div className = "p1">
-                    WHERE YOUR THOUGHTS 
+            <div className="Phrase">
+                <div className="p1">
+                    WHERE YOUR THOUGHTS
                 </div>
 
-                <div className = "content">
-                    <div className = "p2">
+                <div className="content">
+                    <div className="p2">
                         BLOOM
                     </div>
-                    <div className = "bloom">
+                    <div className="bloom">
                         {/* <img src = {Blooming}/> */}
                     </div>
                 </div>
 
-                <div className = "p3">
-                    INTO NOTES! 
+                <div className="p3">
+                    INTO FLOWERFUL NOTES!
                 </div>
 
-                <div className = "pic">
-                    <img src = {wp}/>
+                <div className="pic">
+
+                    <img src={wp} />
                 </div>
 
+                <div className="tasks">
+                    {/* See what's possible with UNote */}
+                </div>
+            </div>
+            <div className="Container">
+                <p className="pc1">Simple on Surface</p>
+                <p className="pc2">Detailed Underneath</p>
+            </div>
+            <div class="wrapper">
+
+                <div class="flip">
+                    <div class="front">
+                        <h1 class="text-shadow">Use it anywhere</h1>
+                    </div>
+                    <div class="back">
+                        <h2>Work Anywhere</h2>
+                        <p>Keep important info handy—your notes sync automatically to all your devices.</p>
+                    </div>
+                </div>
+
+                <div class="flip">
+                    <div class="front">
+                        <h1 class="text-shadow">Use it anywhere</h1>
+                    </div>
+                    <div class="back">
+                        <h2>Work Anywhere</h2>
+                        <p>Keep important info handy—your notes sync automatically to all your devices.</p>
+                    </div>
+                </div>
+
+
+                <div class="flip">
+                    <div class="front">
+                        <h1 class="text-shadow">Use it anywhere</h1>
+                    </div>
+                    <div class="back">
+                        <h2>Work Anywhere</h2>
+                        <p>Keep important info handy—your notes sync automatically to all your devices.</p>
+                    </div>
+                </div>
+
+                {/* <div className="Container1">
+                    <div className="c1">
+                        <p>Use it anywhere</p>
+                        Notes stay updated across all your devices, automatically and in real time.
+                        There’s no “sync” button.It just works.
+                    </div>
+                </div> */}
+                {/* <div className="Container2">
+                    Container
+                </div>
+                <div className="Container3">
+                    Container
+
+                </div> */}
             </div>
         </>
     )
--- Day3/myapp/src/Components/Nav.js
+++ Day3/myapp/src/Components/Nav.js
@@ -1,15 +1,21 @@
 
 
-const Nav =()=>{
+const Nav = () => {
     return (
         <>
-            <div className = "navbar">
-                <div classNmae = "title">
-                    <h1>UNote</h1>
+            <div className="navbar">
+                <div class="patterns">
+                    <svg width="100%" height="100%">
+
+                        <text x="4%" y="7%" text-anchor="middle">
+                            UNote
+                        </text>
+                    </svg>
                 </div>
-                <ul className = "nav">
-                    <li className = "link"><a href="#">Sign In</a></li>
-                    <li className = "link"><a href="#">Sign Up</a></li>
+
+                <ul className="nav">
+                    <li className="link"><a href="#">Sign In</a></li>
+                    <li className="link"><a href="#">Sign Up</a></li>
                 </ul>
             </div>
         </>
